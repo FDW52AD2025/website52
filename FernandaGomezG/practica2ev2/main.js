9ac6ac5e2725c4cb61ec018aca46c60c27a420e5
Fernanda Nohemi Gomez Garcia <fernanda.gomez070206@gmail.com>
2025-10-14 19:57:35 -0600

    const, table

---
+++ FernandaGomezG/practica2ev2/main.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById ("boton");
+const tabla = document.getElementById ("tabla");
+
+boton.addEventListener("click", e => {
+    let contador = 1;
+
+    for( let i = 0; i<=3; i++){
+        const filas = document.createElement("tr");
+        
+    }
+})
