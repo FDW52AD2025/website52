d4dac2deee9e60e5917607bcf43ef568616fffa4
Hugo Alejandro Zapata Rosales <hugoalejandrozapatarosales5@gmail.com>
2025-10-14 19:40:44 -0600

    Práctica 2 finalizada

--- Hugo-0699/ev2_practica-2/main.js
+++ Hugo-0699/ev2_practica-2/main.js
@@ -1,9 +1,24 @@
-const boton = document.getElementById("boton")
 const tabla = document.getElementById("tabla")
+const boton = document.getElementById("boton")
 
 let datos = [1,2,3,4,5,6,7,8,9]
 boton.addEventListener("click", e => {
+    tabla.innerHTML = "";
+    let contador = 0;
 
-    for(;;){
+    //filas
+    for(let fila = 0; fila < 3; fila ++){
+        const tfila = document.createElement("tr");
+
+        //celdas
+        for(let celda = 0; celda < 3; celda ++){
+            const td = document.createElement("td");
+            td.innerText = datos[contador]; 
+            td.style.border = "3px solid black";
+            contador +=1;
+            tfila.appendChild(td);
+        }
+
+        tabla.appendChild(tfila)
     }
 });
