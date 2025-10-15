124f322c8a171c16958bf5aa34103d905821f4af
Fernanda Nohemi Gomez Garcia <fernanda.gomez070206@gmail.com>
2025-10-14 21:21:37 -0600

    estilos

--- FernandaGomezG/practica2ev2/main.js
+++ FernandaGomezG/practica2ev2/main.js
@@ -2,10 +2,19 @@ const boton = document.getElementById ("boton");
 const tabla = document.getElementById ("tabla");
 
 boton.addEventListener("click", e => {
-    let contador = 1;
-
-    for( let i = 0; i<=3; i++){
+    let conten = 1;
+    
+    for(let i = 0; i<=3; i++){
         const filas = document.createElement("tr");
+        
+        for( let i=0; i<=3; i++){
+            const columnas = document.createElement("td");
+            columnas.textContent = conten++;
+            filas.appendChild(columnas)
+            columnas.style.border= "2px solid black"
+            columnas.style.backgroundColor= "lightblue"
+        }
+        tabla.appendChild(filas)
         
     }
 })
